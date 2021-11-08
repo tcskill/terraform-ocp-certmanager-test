84a71742032854173c5e650dadae492892f05d11
Thomas Skill <tskill@us.ibm.com>
2021-11-08 14:06:37 -0600

    updatever

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -1,5 +1,5 @@
 module "dev_cluster" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-ocp?ref=v1.7.0"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-ocp?ref=v1.9.0"
 
   resource_group_name     = var.resource_group_name
   region                  = var.region
